8f0761167071a2167641b572adfa9abe7fc68e2a
Giovanni Simoni <dacav@teknik.io>
2017-12-24 16:42:16 +0100

    simple echo server

--- main.c
+++ main.c
@@ -1,7 +1,16 @@
 #include "setup.h"
+#include "session.h"
 
 #include <stdlib.h>
 #include <errno.h>
+#include <string.h>
+
+static session_t on_accepted(setup_t setup, int clsock);
+static void on_deliver(session_t, uint8_t *, size_t);
+static void on_send_done(session_t);
+static void on_end_of_stream(session_t);
+static void on_error(session_t, char* op, int errno_val);
+static void on_deleted(session_t);
 
 int main (int argc, char **argv)
 {
@@ -9,6 +18,9 @@ int main (int argc, char **argv)
         .ip_bind_address = "::1",
         .tcp_port = 7070,
         .backlog = 3,
+        .max_sessions = 8,
+        .user_context = NULL,
+        .on_accepted = on_accepted
     };
     setup_t setup = setup_new(&setup_conf);
     if (!setup) {
@@ -26,3 +38,73 @@ int main (int argc, char **argv)
     return EXIT_SUCCESS;
 }
 
+static session_t on_accepted(setup_t setup, int clsock)
+{
+    struct session_params params = {
+        .event_base = setup_get_event_base(setup),
+        .socket = clsock,
+        .recv_buffer_size = 512,
+        .on_deliver = on_deliver,
+        .on_send_done = on_send_done,
+        .on_end_of_stream = on_end_of_stream,
+        .on_error = on_error,
+        .on_deleted = on_deleted,
+
+        .send_timeout = {
+            .tv_sec = 1000,
+            .tv_usec = 0
+        },
+        .recv_timeout = {
+            .tv_sec = 1000,
+            .tv_usec = 0
+        },
+        .user_context = setup
+    };
+
+    session_t session = session_new(&params);
+    if (!session) return NULL;
+
+    session_sched_recv(session);
+    return session;
+}
+
+static void on_deliver(session_t session, uint8_t *data, size_t len)
+{
+    fprintf(stderr, "session %p: delivered %zu bytes\n",
+        session, len
+    );
+
+    if (session_send_bytes(session, data, len) == -1) {
+        fprintf(stderr, "session_send_buffer: %s\n", strerror(errno));
+        session_sched_delete(session);
+    }
+}
+
+static void on_send_done(session_t session)
+{
+    fprintf(stderr, "sent. Accepting next.\n");
+    if (session_sched_recv(session) == -1) {
+        fprintf(stderr, "session_sched_recv: %s\n", strerror(errno));
+        session_sched_delete(session);
+    }
+}
+
+static void on_end_of_stream(session_t session)
+{
+    fprintf(stderr, "end_of_stream for session %p\n", session);
+    session_sched_delete(session);
+}
+
+static void on_error(session_t session, char* op, int errno_val)
+{
+    fprintf(stderr, "error for session %p: while %s got %s\n",
+        session, op, strerror(errno_val)
+    );
+    session_sched_delete(session);
+}
+
+static void on_deleted(session_t session)
+{
+    setup_t setup = (setup_t) session_get_context(session);
+    setup_notify_session_termination(setup, session);
+}
